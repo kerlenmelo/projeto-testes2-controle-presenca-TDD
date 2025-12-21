ea53edf8862a6e28a5ae16e073b013da41a4129c
kerlenmelo <kerlen_1@hotmail.com>
2025-12-21 16:04:09 -0300

    chore(backend): remove arquivos legados e ajustar estrutura do backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+
 const chamadaRoutes = require('../backend/routes/ChamadaRoutes');
 const authRoutes = require('../backend/routes/AuthRoutes');
-const app = express();
+const alunoRoutes = require('../backend/routes/AlunoRoutes');
+const professorRoutes = require('../backend/routes/ProfessorRoutes');
+const disciplinaRoutes = require('../backend/routes/DisciplinaRoutes');
 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/chamadas', chamadaRoutes);
 app.use('/api/auth', authRoutes);
-module.exports = app;
+app.use('/api/chamadas', chamadaRoutes);
+app.use('/api/alunos', alunoRoutes);
+app.use('/api/professores', professorRoutes);
+app.use('/api/disciplinas', disciplinaRoutes);
 
+module.exports = app;
