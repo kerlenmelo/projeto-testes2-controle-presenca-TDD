b5c626a47772e498af0be0a602eeab02d066a2d5
kerlenmelo <kerlen_1@hotmail.com>
2025-12-21 15:56:41 -0300

    chore(backend): ajusta configurações e ambiente

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  await mongoose.connect(
-    'mongodb+srv://rm180249:lL8ATaNLr4vCrVkl@controle-presenca.7h1ruog.mongodb.net/?appName=controle-presenca'
-  );
-  console.log('MongoDB conectado');
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB conectado');
+  } catch (error) {
+    console.error('Erro ao conectar no MongoDB:', error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const app = require('../backend/app');
 const connectDB = require('../backend/database');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`API rodando na porta ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`API rodando na porta ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Erro ao conectar no banco:', err);
+    process.exit(1);
   });
-});
